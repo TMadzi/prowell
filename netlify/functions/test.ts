60a7253008ce272f16f9e63ff3a84637cf254d91
TMadzi <tendaimadzi@aol.com>
2022-06-19 21:48:24 +0200

    add prisma

--- netlify/functions/test.ts
+++ netlify/functions/test.ts
@@ -1,6 +1,22 @@
 import type { Handler } from '@netlify/functions'
+import { Client } from 'pg'
+
+const client = new Client(process.env.DATABASE_URL)
 
 const handler: Handler = async (event, context) => {
+  (async () => {
+    await client.connect()
+    try {
+      const results = await client.query('SELECT NOW()')
+      console.log(results)
+    }
+    catch (err) {
+      console.error('error executing query:', err)
+    }
+    finally {
+      client.end()
+    }
+  })()
   return {
     statusCode: 200,
     body: JSON.stringify({ message: 'Hello World' }),
